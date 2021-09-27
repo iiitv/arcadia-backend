51ba504468a3c5f6f981b6bc4c10c4ad70745920
Krishna Gaur <gaurkrishna498@gmail.com>
2021-09-27 19:13:46 +0530

    added organizer model

--- events/models.py
+++ events/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from authentication.models import User
 
 class Event(models.Model):
     name = models.CharField(max_length=255, null=False)
     start_date = models.DateTimeField(null=False)
     end_date = models.DateTimeField(null=False)
+
+class Organizer(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="event_organizer")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="organizer_user")
