a7aba16177c5a3804cd26dab0927ca89075a36c7
rexdivakar <rexdivakar@hotmail.com>
2021-10-06 12:00:05 +0530

    Create serializer for Organizer model #7

--- events/serializers.py
+++ events/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Event
+from .models import Event, Organizer
+
 
 class EventSerializer(ModelSerializer):
     class Meta:
         model = Event
+        fields = "__all__"
+
+
+class OrganizerSerializer(ModelSerializer):
+    class Meta:
+        model = Organizer
         fields = "__all__"
