2b2dce8a061e72f6888c03c884f921e126121552
Joel Hanson <joelhanson025@gmail.com>
2021-10-14 21:44:37 +0530

    New organizers api and new permission class for non safe rest methods
    - A new organizers CURD api added.
    - A new permission class called the SafeMethodPermission to check the non safe method permission added.

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import EventApi
+from .views import EventApi, OrganizerApi
 from rest_framework import routers
 
 router = routers.SimpleRouter()
 router.register(r'events', EventApi)
+router.register(r'organizers', OrganizerApi)
 
 urlpatterns = router.urls
--- events/views.py
+++ events/views.py
@@ -1,36 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
 from .serializers import EventSerializer, OrganizerSerializer
-from .models import Event
+from .models import Event, Organizer
+from .permissions import SafeMethodPermission
 
 
 class EventApi(ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, SafeMethodPermission]
 
-    def create(self, request, *args, **kwargs):
-        if request.user.is_staff or request.user.is_superuser:
-            return super().create(request, *args, **kwargs)
-        else:
-            return Response({"status": "User unauthorized"}, status = status.HTTP_401)
 
-    def update(self, request, *args, **kwargs):
-        if request.user.is_staff or request.user.is_superuser:
-            return super().update(request, *args, **kwargs)
-        else:
-            return Response({"status": "User unauthorized"}, status = status.HTTP_401)
-
-    def partial_update(self, request, *args, **kwargs):
-        if request.user.is_staff or request.user.is_superuser:
-            return super().partial_update(request, *args, **kwargs)
-        else:
-            return Response({"status": "User unauthorized"}, status = status.HTTP_401)
-
-    def destroy(self, request, *args, **kwargs):
-        if request.user.is_staff or request.user.is_superuser:
-            return super().destroy(request, *args, **kwargs)
-        else:
-            return Response({"status": "User unauthorized"}, status = status.HTTP_401)
+class OrganizerApi(ModelViewSet):
+    queryset = Organizer.objects.all()
+    serializer_class = OrganizerSerializer
+    permission_classes = [IsAuthenticated, SafeMethodPermission]
